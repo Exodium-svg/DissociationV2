7cd0531b7966f644fd014d7532e779d1514bda3c
Exodium <Nem56@NEMO-WINDOWS>
2026-07-17 00:43:03 +0200

    Installed required packages.

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,1 +1,12 @@
-﻿    
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    public static async Task Main()
+    {
+
+
+
+        await Process.GetCurrentProcess().WaitForExitAsync();
+    }
+}
